2a5adb9f5a30a23267d283a388c25adda6a4ffd9
Tassha Nakagawa <nakagawatassha@gmail.com>
2025-09-29 21:21:33 +0800

    feat: add navbar

---
+++ src/js/main.js
@@ -0,0 +1,9 @@
+import $ from 'jquery';
+import 'foundation-sites/dist/js/foundation.js';
+
+// Make jQuery globally available (required by Foundation)
+window.$ = $;
+window.jQuery = $;
+
+// Initialize foundation
+$(document).foundation();
