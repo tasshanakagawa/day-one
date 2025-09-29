fb77e491bc5c6139ada491aaf81bd9e162522bde
Tassha Nakagawa <nakagawatassha@gmail.com>
2025-09-30 01:40:01 +0800

    feat: add testimonials

--- src/js/main.js
+++ src/js/main.js
@@ -1,9 +1,20 @@
 import $ from 'jquery';
+import Flickity from 'flickity';
 import 'foundation-sites/dist/js/foundation.js';
 
-// Make jQuery globally available (required by Foundation)
+// Make jQuery globally available
 window.$ = $;
 window.jQuery = $;
 
 // Initialize foundation
 $(document).foundation();
+
+// Initialize testimonials slider
+new Flickity( '.testimonials__slider', {
+  cellAlign: 'left',
+  wrapAround: true,
+  groupCells: true,
+  prevNextButtons: false,
+  pageDots: false,
+  autoPlay: 3000,
+});
